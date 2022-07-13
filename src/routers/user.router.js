4f695eade84af58df5331478d66ef08eef03ff88
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-13 20:04:55 -0300

    feat: Requisito 5 - criada rota "GET /user"

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const rescue = require('express-rescue');
+const auth = require('../middlewares/auth');
 const userController = require('../controllers/user.controller');
 
 const router = express.Router();
 
+router.get('/', auth, userController.getAllUsers);
 router.post('/', rescue(userController.create));
 
 module.exports = router;
