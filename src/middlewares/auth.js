e71d3f0bc5f128c8b3addca49fcee781e5a11e81
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-13 20:02:31 -0300

    feat: Requisito 5 - criado middleware auth para autenticação

---
+++ src/middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const generateError = require('../helpers/generateError');
+require('dotenv').config();
+
+const secret = process.env.JWT_SECRET;
+
+const validateToken = (token) => {
+  if (!token) return generateError('UNAUTHORIZED', 'Token not found');
+
+  const verifyToken = jwt.verify(token, secret);
+
+  return verifyToken;
+};
+
+const auth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  try {
+    const isTokenValid = validateToken(token);
+  
+    if (isTokenValid.error) return next(isTokenValid.error);
+  
+    next();
+  } catch (err) {
+    next(generateError('UNAUTHORIZED', 'Expired or invalid token').error);
+  }
+};
+
+module.exports = auth;
