93f73b7223e95c39ea7b585585c0f3a63ed27f36
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-13 17:21:48 -0300

    feat: Requisito 3 - rotas de login e middleware de erro adionados à API

--- src/api.js
+++ src/api.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const loginRouter = require('./routers/login.router');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/login', loginRouter);
+
+app.use(errorMiddleware);
 
 // ...
 
