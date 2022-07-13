9bcee5ed26f0f625f7ab57ac5aac790226b7455e
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-13 15:34:12 -0300

    feat: Requisito 1 - migration de BlogPosts criado

---
+++ src/database/migrations/20220713175219-create-blog_posts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts',{
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      published: { 
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updated: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('');
+  }
+};
