b74edf0762ac7fee0d25aaa69b1f6ac40c341776
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-13 20:40:30 -0300

    feat: Requisito 7 - criado model category.js

---
+++ src/database/models/category.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    }
+  },
+  {
+    tableName: 'Categories',
+  });
+
+  return Category;
+};
