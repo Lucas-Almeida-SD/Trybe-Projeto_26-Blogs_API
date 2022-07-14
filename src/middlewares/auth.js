db9b8b4f506828e43b4c6379a24a21265ac061a8
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-14 15:33:21 -0300

    refactor: não executa mais o next para enviar o erro de token não encontrado, mas lança uma exceção com o erro

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,8 +5,6 @@ require('dotenv').config();
 const secret = process.env.JWT_SECRET;
 
 const validateToken = (token) => {
-  if (!token) return generateError('UNAUTHORIZED', 'Token not found');
-
   const verifyToken = jwt.verify(token, secret);
 
   return verifyToken;
@@ -15,16 +13,16 @@ const validateToken = (token) => {
 const auth = (req, res, next) => {
   const token = req.headers.authorization;
 
+  if (!token) throw generateError('UNAUTHORIZED', 'Token not found');
+
   try {
     const isTokenValid = validateToken(token);
-  
-    if (isTokenValid.error) return next(isTokenValid.error);
 
     req.user = isTokenValid.data;
   
     next();
-  } catch (err) {
-    next(generateError('UNAUTHORIZED', 'Expired or invalid token').error);
+  } catch (_err) {
+    next(generateError('UNAUTHORIZED', 'Expired or invalid token'));
   }
 };
 
