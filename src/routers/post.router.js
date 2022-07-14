baa41ad658c28fbd933f19348e37399fef6d269c
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-14 16:23:37 -0300

    feat: Requisito 13 - criada rota "GET /post"

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -5,6 +5,7 @@ const postController = require('../controllers/post.controller');
 
 const router = express.Router();
 
+router.get('/', rescue(auth), rescue(postController.getAllPosts));
 router.post('/', rescue(auth), rescue(postController.create));
 
 module.exports = router;
