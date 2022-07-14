d42da7de6bd80a1835c1112b8190d79e4d442ebd
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-14 16:22:59 -0300

    feat: Requisito 13 - criada camada controller para rota "GET /post"

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,6 +1,12 @@
 const httpStatus = require('../helpers/httpStatus');
 const postService = require('../services/post.service');
 
+const getAllPosts = async (req, res) => {
+  const posts = await postService.getAllPosts();
+
+  res.status(httpStatus.OK).json(posts);
+};
+
 const create = async (req, res) => {
   const { title, content, categoryIds } = req.body;
   const { id: userId } = req.user;
@@ -11,5 +17,6 @@ const create = async (req, res) => {
 };
 
 module.exports = {
+  getAllPosts,
   create,
 };
