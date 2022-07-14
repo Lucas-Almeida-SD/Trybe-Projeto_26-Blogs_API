fab85d8c4100e289bef6b105d1f67f8f516ba118
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-13 22:33:18 -0300

    feat: Requisito 9 - criada rota "GET /categories"

--- src/routers/category.router.js
+++ src/routers/category.router.js
@@ -4,6 +4,7 @@ const categoryController = require('../controllers/category.controller');
 
 const router = express.Router();
 
+router.get('/', auth, categoryController.getAllCategories);
 router.post('/', auth, categoryController.create);
 
 module.exports = router;
