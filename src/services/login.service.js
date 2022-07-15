f80a1871aee36baf7e82cbfcdc96b9e3b46ccdae
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-14 21:30:00 -0300

    feat: adicionada função que valida passwords, tanto encriptados como normais

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,7 +1,14 @@
+const bcrypt = require('bcryptjs'); 
 const { User } = require('../database/models');
 const generateError = require('../helpers/generateError');
 const generateToken = require('../helpers/generateToken');
 const httpStatus = require('../helpers/httpStatus');
+
+const validatePassword = (password, passwordInTheDB) => {
+  if (!bcrypt.compareSync(password, passwordInTheDB) && password !== passwordInTheDB) {
+    throw generateError(httpStatus.UNAUTHORIZED, 'Invalid fields');
+  }
+};
 
 const getToken = async (email, password) => {
   if (!email || !password) {
@@ -11,6 +18,8 @@ const getToken = async (email, password) => {
   const user = await User.findOne({ where: { email } });
 
   if (!user) throw generateError(httpStatus.BAD_REQUEST, 'Invalid fields');
+
+  validatePassword(password, user.password);
 
   const token = generateToken(user);
 
