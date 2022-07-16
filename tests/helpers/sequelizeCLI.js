910aa813a6e456ad73060e2eff0798591cc3b463
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-16 18:07:08 -0300

    test: chore: renomenando o arquivo e retirando a variavel de ambiente dos comandos pois não é necessário, já que ela é inserida quando inicia os testes

---
+++ tests/helpers/sequelizeCLI.js
@@ -0,0 +1,6 @@
+module.exports = {
+  drop: 'npx sequelize db:drop',
+  create: 'npx sequelize db:create',
+  migrate: 'npx sequelize db:migrate',
+  seed: 'npx sequelize db:seed:all',
+};
--- tests/helpers/sequelizeCommands.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  drop: 'NODE_ENV=test npx sequelize db:drop',
-  create: 'NODE_ENV=test npx sequelize db:create',
-  migrate: 'NODE_ENV=test npx sequelize db:migrate',
-  seed: 'NODE_ENV=test npx sequelize db:seed:all',
-};
