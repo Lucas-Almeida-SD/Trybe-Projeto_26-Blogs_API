3e8f44911138db7a05a310123ef85148ba88dce0
lucas_almeida <lucas.almdiniz@gmail.com>
2022-07-16 22:39:43 -0300

    test: corrigido mock "allPosts"

--- tests/mocks/posts.js
+++ tests/mocks/posts.js
@@ -3,19 +3,43 @@ const invalidToken = { authorization: 'invalid_token' };
 allPosts = [
   {
     id: 1,
-    title: 'Post do Ano',
-    content: 'Melhor post do ano',
+    title: "Post do Ano",
+    content: "Melhor post do ano",
     userId: 1,
-    published: new Date('2011-08-01T19:58:00.000Z'),
-    updated: new Date('2011-08-01T19:58:51.000Z'),
+    published: "2011-08-01T19:58:00.000Z",
+    updated: "2011-08-01T19:58:51.000Z",
+    user: {
+      id: 1,
+      displayName: "Lewis Hamilton",
+      email: "lewishamilton@gmail.com",
+      image: "https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg"
+    },
+    categories: [
+      {
+        id: 1,
+        name: "Inovação"
+      }
+    ]
   },
-  {
+  { 
     id: 2,
     title: 'Vamos que vamos',
     content: 'Foguete não tem ré',
     userId: 1,
-    published: new Date('2011-08-01T19:58:00.000Z'),
-    updated: new Date('2011-08-01T19:58:51.000Z'),
+    published: "2011-08-01T19:58:00.000Z",
+    updated: "2011-08-01T19:58:51.000Z",
+    user: {
+      id: 1,
+      displayName: "Lewis Hamilton",
+      email: "lewishamilton@gmail.com",
+      image: "https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg"
+    },
+    categories: [
+      {
+        id: 2,
+        name: "Escola"
+      }
+    ]
   },
 ];
 
